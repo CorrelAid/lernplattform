337df1a9c0fdf900d97ef22d1bdcbb748bff2a27
Cosima Meyer <cosima.meyer@gmail.com>
2021-12-05 22:42:05 +0100

    Update with app and app_loesung (for the in-class task)

--- uebungen/shiny/app_loesung.R
+++ uebungen/shiny/app_loesung.R
@@ -23,9 +23,17 @@ community <-
 # wir es `output$balkendiagramm` nennen
 
 server <- function(input, output) {
+    
     # Wir speichern unsere Visualisierung in `output$balkendiagramm`
+    # Nutzt die geeignete Funktion (in diesem Fall renderPlot, da wir einen
+    # Plot darstellen möchten), um den Output zu generieren
+    
     output$balkendiagramm <- renderPlot({
+    
         # Innerhalb von `renderPlot()` kreieren wir unseren ggplot2
+        # Hierfür können wir unseren Code kopieren, den wir in der letzten
+        # Sitzung erstellt haben
+        
         # Wir definieren den Dateninput (`community`)
         community %>%
             # Datenbereinigung
@@ -40,8 +48,12 @@ server <- function(input, output) {
             dplyr::mutate(relative_anzahl = (sum(volunteers) / absolute_anzahl) * 100) %>%
             # Behalten jeden Wert pro Kontinent nur einmal
             distinct(continent, relative_anzahl)  %>%
-            # Als zusätzlichen Schritt müssen wir jetzt noch einen Filter einbauen
+            
+            # Wichtig! Der Filter lief noch nicht - wieso? 
+            # Als zusätzlichen Schritt müssen wir jetzt noch einen Filter 
+            # einbauen, in dem wir auf unsere Auswahl in der `ui` zugreifen
             dplyr::filter(continent %in% input$continentSelection) %>%
+            
             # Initialisieren `ggplot2` und bestimment die x-Achse (Kontinente)
             # und die y-Achse (relative Anzahl an Freiwillien)
             # Zusätzlich legen wir die Farbe der Balken fest
@@ -79,27 +91,58 @@ ui <- fluidPage(
     # Holt euch Inspiration hier: https://rstudio.github.io/shinythemes/
     theme = shinytheme("superhero"),
     
-    # Ändert den Titel
+    # Ändert den Titel in `titelPanel`
     titlePanel("Break Free From Plastic"),
     
     # Definiert die Seitenleiste und den Input
     sidebarLayout(sidebarPanel(
+        # Wir verwenden hier den `checkboxGroupInput`
+        # Das erlaubt uns, ein Input-Menü zu erstellen, bei dem 
+        # wir über "Check-Boxen" die Kontinente auswählen
         checkboxGroupInput(
+            # Gebt dieser Auswahl einen Namen (wir werden den Namen später
+            # nutzen, um auf dieses Auswahlmenü zuzugreifen)
             "continentSelection",
+            # Gebt eine Anleitung für den Nutzer ein (das kann sehr kurz sein
+            # und zum Beispiel "Wähle einen Kontinent" umfassen oder aber auch 
+            # ausführlicher sein - hier seid ihr vollkommen frei in eurer 
+            # Auswahl :-))
             h4("Wähle einen Kontinent"),
+            # Der nächste Schritt ist wichtig - hier definieren wir, was der
+            # Nutzer auswählen kann. Die Logik ist, dass wir eine Liste (`list`)
+            # an Möglichkeiten in `choiches` speichern. Wir möchten hier alle
+            # Kontinente auswählen. Dazu müssen wir jeden Kontinent sein
+            # Anzeigeäquivalent (linke Seite) und seine Bezeichnung im 
+            # Datensatz (rechte Seite) geben
             choices = list(
-                "Americas" = "Americas",
-                "Africa" = "Africa",
-                "Asia" = "Asia",
-                "Europe" = "Europe",
-                "Oceania" = "Oceania"
+                "Nord- und Südamerika" = "Americas",
+                "Afrika" = "Africa",
+                "Asien" = "Asia",
+                "Europa" = "Europe",
+                "Ozeanien" = "Oceania"
             ),
+            # Wir können jetzt noch im letzten Schritt eine Standardvorauswahl 
+            # treffen - hier nehemn wir `"Americas"` (also den Namen im Daten-
+            # satz) :-)
             selected = "Americas"
         )
         
     ),
     
-    # Show a plot of the generated distribution
+    # Fügt das Balkendiagramm in `mainPanel()` ein
+    # Dazu müsst ihr zwei Dinge beachten
+    
+    # 1) Wählt eine geeignete Output-Funktion (hier erhaltet ihr Inspiration 
+    # (https://mastering-shiny.org/basic-ui.html#outputs) oder
+    # schaut auf der Lernplattform in der Übersicht - es handelt sich hierbei um
+    # einen "plot", den wir darstellen möchten)
+    
+    # 2) Fügt das unten generierte "balkendiagramm" ein (es ist wichtig, dass es
+    # in Anführungszeichen steht)
+    
+    # Für die Intuition: Wir greifen hier auf das Objekt ("balkendiagramm") zu, 
+    # das ihr zuvor im `server` generiert habt und lassen es uns ausgeben :-)
+    
     mainPanel(plotOutput("balkendiagramm")))
 )
 
