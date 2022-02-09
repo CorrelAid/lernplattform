3af16940d6bf053cd099904bc1658639598eda18
NinaCorrelAid <nina.h@correlaid.org>
2022-02-09 16:14:11 +0100

    redo testing

--- apptesting.R
+++ apptesting.R
@@ -1,21 +1,21 @@
 library(shinytest)
 
 # Verbindung herstellen
-app <- ShinyDriver$new("https://correlaid.shinyapps.io/lernplattform/")
+app <- shinytest::ShinyDriver$new("https://correlaid.shinyapps.io/lernplattform/")
 app_state <- app$.__enclos_env__$private$state
 if (app_state == "running"){
-  print("Yeah!")
+  # Alles ok
 } else {
-  print("Oh no!")
+  quit(status=1)
 }
 
 # Textbox finden
-app$findElement(xpath = '/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div[2]/div/div[2]/div')$click()
+#app$findElement(xpath = '/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div[2]/div/div[2]/div')$click()
 
 # Code ausführen klicken
-app$findElement(xpath = '/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div[1]/div[2]/a/span')$click()
+#app$findElement(xpath = '/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div[1]/div[2]/a/span')$click()
 
-Sys.sleep(10)
+#Sys.sleep(10)
 
 # Response finden
-app$findElement(xpath = '/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[2]/div/pre/code')
+#app$findElement(xpath = '/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[2]/div/pre/code')
