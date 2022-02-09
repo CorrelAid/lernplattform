34ef7fb18ad1aaf2f6c8601304aad96d2a72c4a4
NinaCorrelAid <nina.h@correlaid.org>
2022-02-09 16:45:28 +0100

    change httr request

--- utils/apptesting.R
+++ utils/apptesting.R
@@ -1,13 +1,25 @@
-library(shinytest)
-
-# Verbindung herstellen
-app <- shinytest::ShinyDriver$new("https://correlaid.shinyapps.io/lernplattform/")
-app_state <- app$.__enclos_env__$private$state
-if (app_state == "running"){
+library(httr)
+app_state <- GET("https://correlaid.shinyapps.io/lernplattform")$status
+if (app_state != 200){
   quit(status=1)
 } else {
-  quit(status=1)
+  # do nothing
 }
+
+
+
+
+### legacy code
+# library(shinytest)
+
+# Verbindung herstellen
+#app <- shinytest::ShinyDriver$new("https://correlaid.shinyapps.io/lernplattform/")
+#app_state <- app$.__enclos_env__$private$state
+#if (app_state == "running"){
+  #quit(status=1)
+#} else {
+  #quit(status=1)
+#}
 
 # Textbox finden
 #app$findElement(xpath = '/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div[2]/div/div[2]/div')$click()
